71054f88dde45a319a087de448d55954f59143d4
Jen Minnich <jminnich@udel.edu>
2018-05-22 19:48:36 -0400

    third commit

--- manage.py
+++ manage.py
@@ -12,14 +12,24 @@ def deploy():
 
     print "inserting initial data..."
     team1 = Team(name='Cleveland Cavaliers', location='Cleveland, OH', team_colors='Maroon and yellow')
-    team2 = Team(name='Philadelphia 76ers', location='Philadelphia, PA', team_colors='Red, white, and blue')
-    player1 = Player(name='LeBron James', age=33, height='6ft 8in', weight='250lbs', jersey_num=23)
-    player2 = Player(name='Joel Embiid', age=24, height='7ft 0in', weight='249lbs', jersey_num=21)
+    team2 = Team(name='Houston Rockets', location='Houston, TX', team_colors='Red, silver, black, white')
+    team3 = Team(name='Golden State Warriors', location='Oakland, CA', team_colors='Royal blue and golden yellow')
+    team4 = Team(name='Boston Celtics', location='Boston, MA', team_colors='Green and white')
+    player1 = Player(name='LeBron James', age=33, height='6ft 8in', weight='250lbs', position="Forward", jersey_num=23, team_id=1)
+    player2 = Player(name='Chris Paul', age=33, height='6ft 0in', weight='175lbs', position="Point guard", jersey_num=3, team_id=2)
+    player3 = Player(name='Stephen Curry', age=30, height='6ft 3in', weight='190lbs', position="Point guard", jersey_num=30, team_id=3)
+    player4 = Player(name='Kyrie Irving', age=26, height='6ft 3in', weight='193lbs', position="Point guard", jersey_num=11, team_id=4)
+    player5 = Player(name='Kevin Love', age=29, height='6ft 10in', weight='251lbs', position="Forward/Center", jersey_num=0, team_id=1)
 
     db.session.add(team1)
     db.session.add(team2)
+    db.session.add(team3)
+    db.session.add(team4)
     db.session.add(player1)
     db.session.add(player2)
+    db.session.add(player3)
+    db.session.add(player4)
+    db.session.add(player5)
 
     db.session.commit()
 
--- nba.py
+++ nba.py
@@ -27,6 +27,7 @@ class Player(db.Model):
     age = db.Column(db.Integer)
     height = db.Column(db.Text)
     weight = db.Column(db.Text)
+    position = db.Column(db.Text)
     jersey_num = db.Column(db.Integer)
     team_id = db.Column(db.Integer, db.ForeignKey('teams.id'))
 
@@ -34,6 +35,11 @@ class Player(db.Model):
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 
 @app.route('/teams')
@@ -48,6 +54,35 @@ def all_players():
     return render_template('all-players.html', players=players)
 
 
+@app.route('/teams/edit/<int:id>', methods=['GET', 'POST'])
+def teams_edit(id):
+    teams = Team.query.filter_by(id=id).first()
+    if request.method == 'GET': # show the form to update
+        return render_template('team-edit.html', teams=teams)
+
+    if request.method == 'POST': # this is update
+        teams.name = request.form['name']
+        teams.location = request.form['location']
+        teams.team_colors = request.form['team_colors']
+        db.session.commit()
+    return redirect(url_for('all_teams'))
+
+
+@app.route('/players/edit/<int:id>', methods=['GET', 'POST'])
+def players_edit(id):
+    players = Player.query.filter_by(id=id).first()
+    if request.method == 'GET': # show the form to update
+        return render_template('player-edit.html', players=players)
+
+    if request.method == 'POST': # this is update
+        players.name = request.form['name']
+        players.age = request.form['age']
+        players.height = request.form['height']
+        players.weight = request.form['weight']
+        players.position = request.form['position']
+        players.jersey_num = request.form['jersey_num']
+        db.session.commit()
+    return redirect(url_for('all_players'))
 
 
 
